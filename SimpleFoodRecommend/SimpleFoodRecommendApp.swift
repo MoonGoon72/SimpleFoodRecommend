d88504360a848305d00d16b8ca06b82c21cf7557
MoonGoon <moongoon.cnu@gmail.com>
2023-02-22 16:57:19 +0900

    Initial Commit

---
+++ SimpleFoodRecommend/SimpleFoodRecommendApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFoodRecommendApp.swift
+//  SimpleFoodRecommend
+//
+//  Created by 문영균 on 2023/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFoodRecommendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
