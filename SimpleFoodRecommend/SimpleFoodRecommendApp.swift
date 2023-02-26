cff1949f36a3c1c260e10bd03efd910297ade52d
MoonGoon <moongoon.cnu@gmail.com>
2023-02-26 23:45:23 +0900

    Update: Launch Screen 2초 딜레이 추가

--- SimpleFoodRecommend/SimpleFoodRecommendApp.swift
+++ SimpleFoodRecommend/SimpleFoodRecommendApp.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 @main
 struct SimpleFoodRecommendApp: App {
+    /// Launch Screen 딜레이주기, but 딜레이주는건 권장하지 않음
+    init() {
+            Thread.sleep(forTimeInterval: 2)
+        }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
